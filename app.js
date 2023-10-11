c254047b45881f959c5f8003cc32dbfee6fbf353
wisperwindoxas <windoxas@gmail.com>
2023-10-11 16:25:46 +0500

    sokme

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const disabledKeys = ["c", "C", "x", "J", "u", "I"]; // keys that will be disabled
+const disabledKeys = ["c", "C", "x", "J", "u", "I"]; // keys 
 const showAlert = e => {
-e.preventDefault(); // preventing its default behaviour
-return alert("Sorry, you can't view or copy source codes this way!");
+e.preventDefault();
+return alert("Tentak uzing yoz 2chi chumo");
 }
 document.addEventListener("contextmenu", e => {
-showAlert(e); // calling showAlert() function on mouse right click
+showAlert(e); 
 });
 document.addEventListener("keydown", e => {
-// calling showAlert() function, if the pressed key matched to disabled keys
+
 if((e.ctrlKey && disabledKeys.includes(e.key)) || e.key === "F12") {
 showAlert(e);
 }
